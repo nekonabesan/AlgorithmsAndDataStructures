e69dcfa03ee655cd53cd624a9bdd0a2398ccb43c
nekonabesan <sheepdogs_2@hotmail.com>
2019-08-18 21:13:55 +0900

    第8講、マージ動作不具合

--- lec008/modules/ad08_03_module.c
+++ lec008/modules/ad08_03_module.c
@@ -4,21 +4,33 @@
 #include <math.h>
 #include <string.h>
 
-void merge(int *a, int left, int sep){
-  if(sep == 1){
+void merge(int *a, int left, int sep, int n, int end){
+  // セパレータが2以下の場合、処理対象外
+  if(sep <= 1){
     return;
   }
+  //printf("%d\n", sep);
   int max = left + (sep * 2);
   int right = left + sep;
+  int j = 0;
   int *buf = (int *)malloc((sep * 2) * sizeof(int));
-  for(int i = left; i < (left + sep); i += 2){
-    buf[i] = a[left + i];
-    buf[i + 1] = a[right + i];
+  for(int i = left; i < end; i+=2){
+    buf[i] = a[i];
+    buf[i + 1] = a[sep + i];
+    //printf("%d\n", i);
+    printf("%s\n","/=====================================/");
+    printf("%d\n", a[i]);
+    printf("%d\n", a[i+1]);
+    printf("%s\n","/=====================================/");
+    //j += 2;
   }
-  for(int i = 0; i < sep; i++){
-    a[left + i] = buf[left + i];
-    a[right + i] = buf[right + i];
+  //printf("%s\n","/=====================================/");
+  for(int i = 0; i < (sep * 2); i++){
+    //printf("%d\n", buf[i]);
+    a[left + i] = buf[i];
+    //a[right + i] = buf[sep + i];
   }
+  //printf("%s\n","/=====================================/");
   free(buf);
 }
 
@@ -31,40 +43,48 @@ void msort(int *a, int sep, int n){
   int end = 0;
   int right = 0;
   int left = 0;
-  if(sep <= 0){
-    return;
+  int mid = n / 2;
+  int k = 0;
+  if(n % 2 != 0){
+    mid += 1;
   }
-  for(int i = 0; i < n; i += (sep * 2)) {
+  for(int i = 0; i < (n - sep); i += (sep * 2)) {
     if(n <= i){
       break;
     }
     end += (sep * 2);
-    for(int j = i; j < (end - sep); j++){
-
+    //printf("end : %d\n", end);
+    //printf("end : %d\n",end);
+    //printf("%s\n","/=====================================/");
+    for(int j = i; j < (i + sep); j+=1){
+      //printf("%s\n","/=====================================/");
+      //printf("i : %d\n", i);
+      //printf("j : %d\n",j);
+      //printf("%s\n","/=====================================/");
       // 右側と左側の検査対象を指すindexを更新する
       left = j;
-      right = j + sep;
-      printf("%d\n",right);
-
+      right = left + sep;
+      //printf("left : %d\n",left);
+      //printf("right : %d\n",right);
       if(end < right){
         if(a[n - 1] < a[left]) {
           tmp_left = a[left];
           tmp_right = a[n - 1];
-          a[left] = tmp_right;
-          a[n - 1] = tmp_left;
+          //a[left] = tmp_right;
+          //a[n - 1] = tmp_left;
         }
         break;
       }
       // 左右比較し入れ替える処理
-      if(a[right] < a[left] && right < n) {
+      if(a[right] < a[left]) {
         tmp_left = a[left];
         tmp_right = a[right];
         a[left] = tmp_right;
         a[right] = tmp_left;
       }
     }
-    merge(a, i, sep);
     printf("%s\n","/=====================================/");
+    merge(a, i, sep, n, end);
   }
 }
 
